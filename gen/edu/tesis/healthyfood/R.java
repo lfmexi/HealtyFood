324ad6144250497309de7d7181acb6b4a56c63cc
lfmexi <lfmexi@gmail.com>
2014-09-25 22:42:05 -0600

    Creación de las Actividades correspondientes a los casos de uso principales

--- gen/edu/tesis/healthyfood/R.java
+++ gen/edu/tesis/healthyfood/R.java
@@ -37,21 +37,39 @@ public final class R {
         public static final int textView3=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_menu_principal=0x7f030002;
+        public static final int activity_busca_recetas=0x7f030000;
+        public static final int activity_ejercicios=0x7f030001;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_medidor=0x7f030004;
+        public static final int activity_menu_principal=0x7f030005;
+        public static final int activity_mis_recetas=0x7f030006;
+        public static final int activity_perfil=0x7f030007;
+        public static final int activity_publica_receta=0x7f030008;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int menu_principal=0x7f070002;
+        public static final int busca_recetas=0x7f070000;
+        public static final int ejercicios=0x7f070001;
+        public static final int login=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int medidor=0x7f070004;
+        public static final int menu_principal=0x7f070005;
+        public static final int mis_recetas=0x7f070006;
+        public static final int perfil=0x7f070007;
+        public static final int publica_receta=0x7f070008;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_busca_recetas=0x7f050005;
+        public static final int title_activity_ejercicios=0x7f050008;
         public static final int title_activity_login=0x7f050003;
+        public static final int title_activity_medidor=0x7f05000a;
         public static final int title_activity_menu_principal=0x7f050004;
+        public static final int title_activity_mis_recetas=0x7f050007;
+        public static final int title_activity_perfil=0x7f050009;
+        public static final int title_activity_publica_receta=0x7f050006;
     }
     public static final class style {
         /** 
--- src/edu/tesis/healthyfood/MenuPrincipal.java
+++ src/edu/tesis/healthyfood/MenuPrincipal.java
@@ -2,12 +2,14 @@ package edu.tesis.healthyfood;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MenuPrincipal extends Activity {
 
@@ -39,7 +41,26 @@ public class MenuPrincipal extends Activity {
 	}
 	
 	private void listaOnClick(View view){
-		
+		String nombreMenu=((TextView) view).getText().toString();
+		if(nombreMenu.equals("Publicar receta")){
+			Intent i = new Intent(this,PublicaReceta.class);
+			this.startActivity(i);
+		}else if(nombreMenu.equals("Buscar recetas")){
+			Intent i = new Intent(this,BuscaRecetas.class);
+			this.startActivity(i);
+		}else if(nombreMenu.equals("Mis recetas")){
+			Intent i = new Intent(this,MisRecetas.class);
+			this.startActivity(i);
+		}else if(nombreMenu.equals("Ejercicios recomendados")){
+			Intent i = new Intent(this,Ejercicios.class);
+			this.startActivity(i);
+		}else if(nombreMenu.equals("Mi Perfil")){
+			Intent i = new Intent(this,Perfil.class);
+			this.startActivity(i);
+		}else if(nombreMenu.equals("Medidor de calorías")){
+			Intent i = new Intent(this,Medidor.class);
+			this.startActivity(i);
+		}
 	}
 	
 	private ListView listaMenu;
