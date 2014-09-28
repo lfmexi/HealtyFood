4e51a3f785a3b7fb6bfad08400e4cc8d5d90436d
lfmexi <lfmexi@gmail.com>
2014-09-27 20:26:19 -0600

    Creación del módulo de publicación de recetas

--- gen/edu/tesis/healthyfood/R.java
+++ gen/edu/tesis/healthyfood/R.java
@@ -8,6 +8,9 @@
 package edu.tesis.healthyfood;
 
 public final class R {
+    public static final class array {
+        public static final int categories_array=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,17 +27,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
-        public static final int listaMenu=0x7f080009;
-        public static final int loginPassword=0x7f080004;
-        public static final int loginUsername=0x7f080002;
-        public static final int login_boton_register=0x7f080007;
-        public static final int login_forgot=0x7f080006;
-        public static final int main_boton_ingreso=0x7f080005;
-        public static final int scrollView1=0x7f080000;
-        public static final int textView1=0x7f080008;
-        public static final int textView2=0x7f080001;
-        public static final int textView3=0x7f080003;
+        public static final int action_settings=0x7f090010;
+        public static final int editText1=0x7f09000f;
+        public static final int listaMenu=0x7f090009;
+        public static final int loginPassword=0x7f090004;
+        public static final int loginUsername=0x7f090002;
+        public static final int login_boton_register=0x7f090007;
+        public static final int login_forgot=0x7f090006;
+        public static final int main_boton_ingreso=0x7f090005;
+        public static final int receta_boton_agrega=0x7f09000c;
+        public static final int receta_imagen=0x7f09000a;
+        public static final int receta_nombre=0x7f09000b;
+        public static final int receta_publicar=0x7f09000e;
+        public static final int receta_spinner_cat=0x7f09000d;
+        public static final int scrollView1=0x7f090000;
+        public static final int textView1=0x7f090008;
+        public static final int textView2=0x7f090001;
+        public static final int textView3=0x7f090003;
     }
     public static final class layout {
         public static final int activity_busca_recetas=0x7f030000;
@@ -48,15 +57,15 @@ public final class R {
         public static final int activity_publica_receta=0x7f030008;
     }
     public static final class menu {
-        public static final int busca_recetas=0x7f070000;
-        public static final int ejercicios=0x7f070001;
-        public static final int login=0x7f070002;
-        public static final int main=0x7f070003;
-        public static final int medidor=0x7f070004;
-        public static final int menu_principal=0x7f070005;
-        public static final int mis_recetas=0x7f070006;
-        public static final int perfil=0x7f070007;
-        public static final int publica_receta=0x7f070008;
+        public static final int busca_recetas=0x7f080000;
+        public static final int ejercicios=0x7f080001;
+        public static final int login=0x7f080002;
+        public static final int main=0x7f080003;
+        public static final int medidor=0x7f080004;
+        public static final int menu_principal=0x7f080005;
+        public static final int mis_recetas=0x7f080006;
+        public static final int perfil=0x7f080007;
+        public static final int publica_receta=0x7f080008;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -93,10 +102,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/edu/tesis/healthyfood/PublicaReceta.java
+++ src/edu/tesis/healthyfood/PublicaReceta.java
@@ -3,6 +3,11 @@ package edu.tesis.healthyfood;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class PublicaReceta extends Activity {
 
@@ -10,6 +15,14 @@ public class PublicaReceta extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_publica_receta);
+		
+		selector_categoria = (Spinner)this.findViewById(R.id.receta_spinner_cat);
+		
+		ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,
+		        R.array.categories_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		selector_categoria.setAdapter(adapter);
 	}
 
 	@Override
@@ -18,5 +31,12 @@ public class PublicaReceta extends Activity {
 		getMenuInflater().inflate(R.menu.publica_receta, menu);
 		return true;
 	}
+	
+	private Spinner selector_categoria;
+	private EditText campo_nombre;
+	private EditText campo_instrucciones;
+	private ImageView imagen;
+	private Button boton_ingredientes;
+	private Button boton_registrar;
 
 }
