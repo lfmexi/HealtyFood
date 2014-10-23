67aea42aba34eb3471c9e1ff4f70e7625cd17c9d
angelboxes <angel.cj.o@gmail.com>
2014-10-23 15:40:48 -0600

    prueba

--- gen/edu/tesis/healthyfood/R.java
+++ gen/edu/tesis/healthyfood/R.java
@@ -26,10 +26,11 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_action_assignment=0x7f020000;
         public static final int ic_action_action_perm_identity=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int ic_launcher_imagen=0x7f020003;
-        public static final int ic_launcher_start=0x7f020004;
-        public static final int ic_stat_icono_camara=0x7f020005;
+        public static final int ic_action_action_settings=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_launcher_imagen=0x7f020004;
+        public static final int ic_launcher_start=0x7f020005;
+        public static final int ic_stat_icono_camara=0x7f020006;
     }
     public static final class id {
         public static final int action_compose=0x7f090024;
@@ -150,6 +151,7 @@ public final class R {
         /**  Application theme. 
          */
         public static final int AppTheme=0x7f070002;
+        public static final int NoTitleText=0x7f070003;
         public static final int estilo=0x7f070001;
     }
 }
