5b1f5b77b872317e1592150b81d7db50125fe15b
angelboxes <angel.cj.o@gmail.com>
2014-10-23 12:41:41 -0600

    cree un menu, no se si sirve :p

--- gen/edu/tesis/healthyfood/R.java
+++ gen/edu/tesis/healthyfood/R.java
@@ -24,12 +24,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_launcher_imagen=0x7f020001;
-        public static final int ic_launcher_start=0x7f020002;
-        public static final int ic_stat_icono_camara=0x7f020003;
+        public static final int ic_action_action_assignment=0x7f020000;
+        public static final int ic_action_action_perm_identity=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_launcher_imagen=0x7f020003;
+        public static final int ic_launcher_start=0x7f020004;
+        public static final int ic_stat_icono_camara=0x7f020005;
     }
     public static final class id {
+        public static final int action_compose=0x7f090024;
+        public static final int action_search=0x7f090023;
         public static final int action_settings=0x7f090021;
         public static final int botonBuscar=0x7f090002;
         public static final int fav_lista=0x7f090004;
@@ -92,12 +96,13 @@ public final class R {
         public static final int main=0x7f080006;
         public static final int medidor=0x7f080007;
         public static final int menu_principal=0x7f080008;
-        public static final int mis_recetas=0x7f080009;
-        public static final int perfil=0x7f08000a;
-        public static final int publica_receta=0x7f08000b;
-        public static final int recetas_propias=0x7f08000c;
-        public static final int registrar=0x7f08000d;
-        public static final int registro=0x7f08000e;
+        public static final int mimenu=0x7f080009;
+        public static final int mis_recetas=0x7f08000a;
+        public static final int perfil=0x7f08000b;
+        public static final int publica_receta=0x7f08000c;
+        public static final int recetas_propias=0x7f08000d;
+        public static final int registrar=0x7f08000e;
+        public static final int registro=0x7f08000f;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
