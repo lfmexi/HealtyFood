24348ecfec0b84aa2c23a042dcfcf80002b27a66
Luis Fernando Morales <lfmexi@gmail.com>
2014-11-15 18:29:47 -0600

    Visualización de las recetas propias completada

--- src/edu/tesis/healthyfood/RecetasPropias.java
+++ src/edu/tesis/healthyfood/RecetasPropias.java
@@ -1,15 +1,60 @@
 package edu.tesis.healthyfood;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 public class RecetasPropias extends Activity {
 
+	private String user="";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recetas_propias);
+		Intent i =getIntent();
+		user = i.getExtras().getString("infoUser");
+		
+		lista = (ListView)this.findViewById(R.id.listaPropias);
+		
+		lista.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				listaOnClick(arg1);
+			}
+		});
+		
+		new BuscaPropiaAsync(this).execute(user);
 	}
 
 	@Override
@@ -17,6 +62,91 @@ public class RecetasPropias extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.recetas_propias, menu);
 		return true;
+	}
+	
+	private void listaOnClick(View v){
+		String nombre = ((TextView) v).getText().toString();
+		Intent i = new Intent(this,VisualizaReceta.class);
+		i.putExtra("infoUser", user);
+		i.putExtra("receta", nombre);
+		this.startActivity(i);
+	}
+	
+	private ListView lista;
+	
+	private class BuscaPropiaAsync extends AsyncTask<String,Void,String[]>{
+		RecetasPropias padre;
+
+		public BuscaPropiaAsync(RecetasPropias r){
+			padre = r;
+		}
+		
+		@Override
+		protected String[] doInBackground(String... arg0) {
+			// TODO Auto-generated method stub
+			HttpClient cliente = new DefaultHttpClient();
+			HttpPost post = new HttpPost(Login.url+"/getUserReceta.php");
+			
+	        List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+	        params.add(new BasicNameValuePair("user",arg0[0]));
+	        try {
+				post.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return getValores(cliente,post);
+		}
+	
+		private StringBuilder inputStreamToString(InputStream is) {
+		    String rLine = "";
+		    StringBuilder answer = new StringBuilder();
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+		    try {
+		     while ((rLine = rd.readLine()) != null) {
+		      answer.append(rLine);
+		       }
+		    }
+		    catch (IOException e) {
+		        e.printStackTrace();
+		     }
+		    return answer;
+		}
+		
+		private String []getValores(HttpClient cliente, HttpPost post){
+			String []regs=null;
+			try{
+		    	HttpResponse response = cliente.execute(post);
+		    	String jsonResult = inputStreamToString(response.getEntity().getContent()).toString();
+		    	JSONArray mArray = new JSONArray(jsonResult);
+		    	int num_registros=mArray.length();
+		    	if(num_registros>0){
+		    		regs = new String[num_registros];
+		    		for (int i = 0; i < num_registros; i++) {
+			    	    JSONObject object = mArray.getJSONObject(i);
+			    	    regs[i] = object.getString("receta");
+			    	}
+		    	}
+		    }catch(JSONException e){
+		    	e.printStackTrace();
+		    } catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    	cliente.getConnectionManager().shutdown();
+			return regs;
+		}
+		
+		protected void onPostExecute(String[] result){
+			if(result!=null){
+				String adaptador[] = new String[result.length];
+				for(int i = 0;i<result.length;i++){
+					adaptador[i] = result[i];
+				}
+				padre.lista.setAdapter(new ArrayAdapter<String>(padre,android.R.layout.simple_list_item_1,android.R.id.text1,adaptador));
+			}
+		}
 	}
 	
 }
