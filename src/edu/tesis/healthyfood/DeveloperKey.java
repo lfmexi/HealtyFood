1a560f62931f0004a198705188987444b6cb74da
Luis Fernando <lfmexi@gmail.com>
2015-03-05 23:54:13 -0600

    Eliminación de todos los archivos de eclipse

--- src/edu/tesis/healthyfood/DeveloperKey.java
+++
@@ -1,5 +0,0 @@
-package edu.tesis.healthyfood;
-
-public class DeveloperKey {
-	public static final String DEVELOPER_KEY ="AIzaSyDUslnlJb0zuXMRXz_61HSLKUQm6tbPEbg";
-}
--- src/edu/tesis/healthyfood/sqlite/TMB.java
+++
@@ -1,6 +0,0 @@
-package edu.tesis.healthyfood.sqlite;
-
-public class TMB {
-	public String fecha_tomado;
-	public double value;
-}
